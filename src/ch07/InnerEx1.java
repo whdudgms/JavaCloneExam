4184bf9265a6fb56997c552d57c2d7987c94f8bd
whdudgms <whdudgms123@naver.com>
2023-11-03 11:12:01 +0900

    Inner 클래스

---
+++ src/ch07/InnerEx1.java
@@ -0,0 +1,35 @@
+package ch07;
+
+public class InnerEx1 {
+    class InstanceInner{
+        int iv = 100;
+        //static int cv =100; 에러! static변수를 선언할 수 없다.
+        final static int CONST =100;
+    }
+
+    static class StaticInner{
+        int iv = 200;
+        static int cv = 200;
+    }
+
+    void Mymethod(){
+        class LocalInner{
+            int iv = 300;
+            // static int cv =300;
+            final static int CONST = 300;
+        }
+    }
+
+    public static void main(String args[]){
+        //같은 클래스라는 상황 고려해야함.!
+
+        //상수에 접근할 경우 그냥 접근 가능.
+        System.out.println(InstanceInner.CONST);
+        System.out.println(StaticInner.cv);
+        // class 안에 instance클래스의 iv 값 참조시 객체 일일이 다 생성 후 접근
+        System.out.println(new InnerEx1().new InstanceInner().iv);
+        //static클래스의 iv 접근시 바로 객체 생성 후에 사용
+        System.out.println(new StaticInner().iv);
+
+    }
+}
