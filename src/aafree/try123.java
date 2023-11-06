1449eb1839d9179723e398844cc14bf8021474dd
whdudgms <whdudgms123@naver.com>
2023-11-06 17:16:28 +0900

    예외처리에 대한 공부

--- src/aafree/try123.java
+++ src/aafree/try123.java
@@ -1,12 +1,29 @@
 package aafree;
 
 public class try123 {
+
+
+    int x = 123;
+
+    //int x = 11; 중복 선언 불가능 자바스크립트는 가능.
+
     public static void main(String[] agrs){
         boolean kk = true;
-        boolean kk1 = kk &&false;
+        boolean kk1 = kk && false;
 
         //자바스크립트랑 다르다. 자바스크립에서 실행되는게 안 될 수 있당!
 
         System.out.println();
+
+        new try123().asdasd();
+    }
+    // 메서드에서 클래스의 멤버변수 참조
+    // 자바는 클래스로 이루어져있고 멤버변수 메서드 생성자 초기화 블럭 등으로 구성
+    void asdasd(){
+        int x = 3;
+        //lv에 접근
+        System.out.println(x);
+        //this.iv에 접근
+        System.out.println(this.x);
     }
 }
---
+++ src/ch08/ExceptionEx1.java
@@ -0,0 +1,15 @@
+package ch08;
+
+public class ExceptionEx1 {
+    public static void main(String[] args){
+        try{
+            try{} catch (Exception e){}
+        }catch (Exception e){
+            try{} catch (Exception e1){}
+        }
+
+        try{
+        }catch (Exception e){}
+
+    }
+}
