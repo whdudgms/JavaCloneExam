e842d653c0217ac411688f4570fb6b43b28e11fa
whdudgms <whdudgms123@naver.com>
2023-11-07 14:35:25 +0900

    exception예외 대한 설명

---
+++ src/ch08/ExceptionEx10.java
@@ -0,0 +1,8 @@
+package ch08;
+
+public class ExceptionEx10 {
+    public static void main(String[] args){
+        //Exception이 1발생할 수 있거나 2발생하면 컴파일 조차 불가능.
+        //throw new Exception();
+    }
+}
