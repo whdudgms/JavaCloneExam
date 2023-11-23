24163619517bf5661509fad8fb6593283fc3084d
whdudgms <whdudgms123@naver.com>
2023-11-23 16:29:47 +0900

    커밋

---
+++ src/ch14/CollectorEx1.java
@@ -0,0 +1,55 @@
+package ch14;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+public class CollectorEx1 {
+    public static void main(String[] args) {
+        String[] strArr = {"aaa", "bbb", "ccc"};
+        Stream<String> strStream = Stream.of(strArr);
+
+        String result = strStream.collect(new ConcatCollector());
+
+        System.out.println(Arrays.toString(strArr));
+        System.out.println("resutl ="+result);
+    }
+}
+
+class ConcatCollector implements Collector<String, StringBuilder, String> {
+
+    @Override
+    public Supplier<StringBuilder> supplier(){
+        return () -> new StringBuilder();
+    }
+
+    @Override
+    public BiConsumer<StringBuilder,String> accumulator(){
+        return (sb, s) -> sb.append(s);
+    }
+
+    @Override
+    public Function<StringBuilder, String> finisher(){
+        return sb -> sb.toString();
+    }
+
+    @Override
+    public BinaryOperator<StringBuilder> combiner(){
+        return (sb,sb2)->{
+            sb.append(sb2);
+            return sb;
+        };
+    }
+
+
+    @Override
+    public Set<Characteristics> characteristics(){
+        return Collections.emptySet();
+    }
+}
